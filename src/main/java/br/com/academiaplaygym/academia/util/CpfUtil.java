8f27e946d4750c6bb9f0ce6a18f77ca2aae7cc6a
Luana <luana.sousa.silva6@gmail.com>
2024-02-15 23:07:53 -0300

    Exclusão de lógica anterior de validação de CPF

--- src/main/java/br/com/academiaplaygym/academia/util/CpfUtil.java
+++ src/main/java/br/com/academiaplaygym/academia/util/CpfUtil.java
@@ -2,26 +2,6 @@ package br.com.academiaplaygym.academia.util;
 
 public class CpfUtil {
 
-    //public static boolean isCPFValido(String cpf) {
-		/*
-		 * cpf = cpf.replaceAll("[^0-9]", ""); // Remove caracteres não numéricos if
-		 * (cpf.length() != 11 || cpf.matches(cpf.charAt(0) + "{11}")) return false;
-		 * 
-		 * int soma = 0; for (int i = 0; i < 9; i++) soma += (cpf.charAt(i) - '0') * (10
-		 * - i); int resultado = soma % 11; if (resultado == 1 || resultado == 0) { if
-		 * (cpf.charAt(9) != '0') return false; } else if (cpf.charAt(9) - '0' != 11 -
-		 * resultado) return false;
-		 * 
-		 * soma = 0; for (int i = 0; i < 10; i++) soma += (cpf.charAt(i) - '0') * (11 -
-		 * i); resultado = soma % 11; if (resultado == 1 || resultado == 0) { if
-		 * (cpf.charAt(10) != '0') return false; } else if (cpf.charAt(10) - '0' != 11 -
-		 * resultado) return false;
-		 * 
-		 * return true;
-		 */
-    	
-        // Remove todos os caracteres não numéricos
-        
 	public static String formatarCPF(String cpf) {
         // Remove todos os caracteres não numéricos
         cpf = cpf.replaceAll("[^0-9]", "");
