4b95aab8b803c1054afac144bdada1ac5c906925
Luana <luana.sousa.silva6@gmail.com>
2024-12-26 11:45:37 -0300

    Adjusts formatting logic in the create and update student methods.

--- src/main/java/br/com/academiaplaygym/academia/repository/AlunoRepository.java
+++ src/main/java/br/com/academiaplaygym/academia/repository/AlunoRepository.java
@@ -1,9 +1,11 @@
 package br.com.academiaplaygym.academia.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.academiaplaygym.academia.model.Aluno;
 
-public interface AlunoRepository<UUID> extends JpaRepository<Aluno, String>{
+public interface AlunoRepository extends JpaRepository<Aluno, UUID>{
 
 }
